44b1635970ff3d36875be3e961b67087656a9de9
Abdulaziz Jonuzokov <aajonuzokov@gmail.com>
2025-12-24 18:53:09 +0500

    top_paying_skills

---
+++ 5_optimal_skills.sql
@@ -0,0 +1,16 @@
+SELECT
+    s.skills AS skill_name,
+    COUNT(s.skills) AS skill_count,
+    ROUND(AVG(jpf.salary_year_avg),0) AS avarage_salary
+FROM
+    skills_dim AS s
+INNER JOIN skills_job_dim AS sj ON sj.skill_id = s.skill_id
+INNER JOIN job_postings_fact AS jpf ON jpf.job_id = sj.job_id
+WHERE
+    jpf.job_title_short = 'Data Analyst' 
+    AND jpf.salary_year_avg IS NOT NULL
+    --AND jpf.job_work_from_home = TRUE
+GROUP BY
+    skill_name
+ORDER BY
+    avarage_salary DESC;
--- sql_project/3_top_demanded_skills.sql
+++ sql_project/3_top_demanded_skills.sql
@@ -6,9 +6,9 @@ FROM
 LEFT JOIN skills_job_dim AS sj ON sj.job_id = jpf.job_id
 LEFT JOIN skills_dim AS s ON s.skill_id = sj.skill_id
 WHERE
-    jpf.job_title_short = 'Data Analyst'
+    jpf.job_title_short = 'Data Analyst' AND
+    jpf.salary_year_avg IS NOT NULL
 GROUP BY
     skill_name
 ORDER BY
-    skill_count DESC
-LIMIT 5;
+    skill_count DESC;
